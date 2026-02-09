256b6a38bac64c09b17dce1e572b382eccf26eaf
Sharavi Shinde <sharavishinde@Sharavis-MacBook-Air.local>
2026-02-09 12:14:57 +0530

    All functions are calculated.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #calculator
+
+import addition as a
+import subtraction as s
+import multiplication as m
+
+print("Addition result=", a.add(5,4))
+print("Subtraction result=", s.subtraction(9,5))
+print("Multiplication result=", m.multiplication(10,7))
