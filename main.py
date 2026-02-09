cbaace949e6df6f48c6f82c8a9be9152e8b60fae
Sharavi Shinde <sharavisshinde03@gmail.com>
2026-02-09 12:29:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 import addition as a
 import subtraction as s
 import multiplication as m
-
+#calling functions
 print("Addition result=", a.add(5,4))
 print("Subtraction result=", s.subtraction(9,5))
 print("Multiplication result=", m.multiplication(10,7))
+#this is cslc
