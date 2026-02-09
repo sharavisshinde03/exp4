758f4e94cf1452b40ca0dda4bdc5c4210839e131
Sharavi Shinde <sharavishinde@Sharavis-MacBook-Air.local>
2026-02-09 12:42:21 +0530

    Division added.

--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 import addition as a
 import subtraction as s
 import multiplication as m
+import division as d
 #calling functions
 print("Addition result=", a.add(5,4))
 print("Subtraction result=", s.subtraction(9,5))
 print("Multiplication result=", m.multiplication(10,7))
+print("Division result=",d.div(3,6))
 #this is cslc
